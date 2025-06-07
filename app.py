90eafdadf4faf8c8536a67f56644be2f47272e2d
Jesica Balmaceda <itatibalmacedajesi@gmail.com>
2025-06-07 10:57:06 -0300

    Actualización en app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///encuesta.db'
+import os
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
